4cca28a1da7b78857f1295139fd719dd67ee1e0b
Tariqul Islam <usukuk@gmail.com>
2022-04-18 19:12:06 +0600

    fix minor responsiveness issue

--- src/components/ControlledCarousel.jsx
+++ src/components/ControlledCarousel.jsx
@@ -17,13 +17,9 @@ function ControlledCarousel() {
         <Carousel.Item key={index}>
           <img className="d-block w-100" src="banner.jpg" alt="First slide" />
           <Carousel.Caption>
-            <h2 className="hero-heading fw-bold mb-3">
-              Find Your Ideal Career
-            </h2>
-            <p className="">
-              Are you stuck in a job you don't enjoy? Not sure which career
-              direction to take? If you want to have a job you are passionate
-              about, well, you can!
+            <h2 className="hero-heading fw-bold mb-3">Ideal Career</h2>
+            <p className="mb-0">
+              If you want to have a job you are passionate about, well, you can!
             </p>
           </Carousel.Caption>
         </Carousel.Item>
